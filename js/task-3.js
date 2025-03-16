85414a98421641cf78cf4fe05c3fe60823aec46b
Maryna <marinailukova01@gmail.com>
2025-03-16 14:56:39 +0200

    update js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-  return totalWidth;
+function filterArray(numbers, value) {
+  let newNumbers = [];
+  for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element > value) {
+      newNumbers.push(element);
+    }
+  }
+  return newNumbers;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
